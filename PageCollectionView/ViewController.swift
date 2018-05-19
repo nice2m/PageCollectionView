0d0767ab94a9504287df90da715a559f53690dc8
xiedacai <xiedacai@vcredit.com>
2018-05-19 14:02:10 +0800

    Initial Commit

---
+++ PageCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageCollectionView
+//
+//  Created by vcredit on 2018/5/19.
+//  Copyright © 2018年 vcredit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
